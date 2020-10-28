4d37f59e5c96bd451612cecbfb59d72513fc8238
JosephCHW <57486230+JosephCHW@users.noreply.github.com>
2020-10-28 18:24:33 +0000

    Constructors

---
+++ Constructors/src/London.java
@@ -0,0 +1,10 @@
+
+public class London {
+	public void welcome() {
+		System.out.println("Welcome to London");
+	}
+	public London() { // no return type, i.e. don't write void
+		System.out.println("This is the constructor");
+	}
+}
+
---
+++ Constructors/src/Start.java
@@ -0,0 +1,7 @@
+
+public class Start {
+	public static void main(String abc[]) {
+		London ref;
+		ref = new London();
+	}
+}
